ac0aaf85a78317fe44b0b95046a9fe93c3f8a29f
saumyasingh9 <bajrangbali246new@gmail.com>
2025-11-10 17:39:58 +0530

    update bucket

--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -64,7 +64,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0150ccaf51ab55a51" // Replace with the latest AMI ID for your region
+  default     = "ami-0157af9aea2eef346" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
